618a922b20730a7bbc0ed5a38b8d10e872ad468f
Lorenzo Torres <lorenzoajt@gmail.com>
2023-09-09 15:13:14 -0600

    v 0.0.1 ready

---
+++ test/data/products.ts
@@ -0,0 +1,12 @@
+export const product1 = {
+  id: '1',
+  title: 'Coffee Mug - Card',
+  // img: "./coffee-mug.png",
+};
+
+export const product2 = {
+    id: '1',
+    title: 'Coffee Mug - Card con Imagen',
+    img: "./coffee-mug.png",
+  };
+  
